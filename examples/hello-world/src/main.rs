d4259be8cdd96c5f1563847da36a7bfc8822f625
Sam Bonill <sam@MacBook-Pro-de-Sam-2.local>
2022-10-17 16:58:28 -0500

    hello-world example

---
+++ examples/hello-world/src/main.rs
@@ -0,0 +1,12 @@
+use graphul::{Graphul, http::Methods};
+
+#[tokio::main]
+async fn main() {
+    let mut app = Graphul::new();
+
+    app.get("/", || async {
+        "Hello, World 👋!"
+    });
+
+    app.run("127.0.0.1:8000").await;
+}
