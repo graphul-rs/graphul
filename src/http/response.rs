deef84f5c437a2da2c714ff6d8861eee7bee0af2
Sam Bonill <sam@MacBook-Pro-de-Sam-2.local>
2022-10-21 18:05:54 -0500

    remove unnecessary test case

--- src/http/response.rs
+++ src/http/response.rs
@@ -2,12 +2,3 @@ pub type Response = axum::response::Response;
 pub type Value = serde_json::Value;
 
 pub use axum::response::{Form, Html, Redirect};
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
