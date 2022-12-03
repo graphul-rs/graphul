b392b570ef9ceb8f48480cf3f1a583997db8d266
Sam Bonill <pythonners@gmail.com>
2022-12-03 08:25:32 -0500

    add from_fn_with_state

--- src/middleware/mod.rs
+++ src/middleware/mod.rs
@@ -1,6 +1,6 @@
 use crate::Body;
 
-pub use axum::middleware::{self, from_fn};
+pub use axum::middleware::{self, from_fn, from_fn_with_state};
 
 pub type Next = middleware::Next<Body>;
 pub use tower_http as tower;
