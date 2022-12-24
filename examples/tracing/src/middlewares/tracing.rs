c02f67bf76fe14622f2978fd8223c4faaf90e492
Phosphorus <fernando.n.p.d.moscu@gmail.com>
2022-12-24 03:01:19 -0300

    docs: add an example using tracing

---
+++ examples/tracing/src/middlewares/tracing.rs
@@ -0,0 +1,30 @@
+use std::time::Instant;
+
+use graphul::{http::response::Response, middleware::Next, Req};
+use tracing::{Instrument, Level};
+
+pub async fn tracing_middleware(request: Req, next: Next) -> Response {
+    let span = tracing::span!(
+        Level::INFO,
+        "Request",
+        path = request.uri().to_string(),
+        version = ?request.version(),
+        method = request.method().to_string()
+    );
+    // your logic
+
+    async move {
+        let now = Instant::now();
+        let response = next.run(request).await;
+        let duration = now.elapsed();
+
+        tracing::info!(
+            status = response.status().to_string(),
+            duration = ?duration,
+            "Response"
+        );
+        response
+    }
+    .instrument(span)
+    .await
+}
